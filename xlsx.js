f124fc8468d95b7c68c817c7752d1f4de02e98c2
logos333 <temur_333@mail.ru>
2023-09-26 16:15:44 +0800

    Update xlsx.js
    fix QUOTE error

--- xlsx.js
+++ xlsx.js
@@ -21521,7 +21521,7 @@ var XmlNode = (function () {
     return this;
   }
 
-  var APOS = "'"; QUOTE = '"'
+  var APOS = "'", QUOTE = '"'
   var ESCAPED_QUOTE = {  }
   ESCAPED_QUOTE[QUOTE] = '&quot;'
   ESCAPED_QUOTE[APOS] = '&apos;'
